e8cf4cc8696bbbf76df93d3491a8e7062f50ef12
Olga Garcia <olgarciaq11@gmail.com>
2025-10-07 14:45:24 +0200

    Faltan las fotos

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,9 +4,12 @@ import Image from 'next/image';
 
 export default function header() {
   return (
-    <header>
+    <header id="cabecera" className="flex flex-col items-center justify-center font-sixtyfour bg-slate-800">
         <Image src={logoHeader} alt="React Concepts" width={150}></Image>
-        <div>Header</div>
+        <h1 className="w-5/6 text-center text-3xl font-extrabold bg-gradient-to-r from-purple-700 to-violet-300 bg-clip-text text-transparent">
+        Conceptos básicos de React
+        </h1>
+        <h2 className="w-5/6 m-4 text-x1 text-slate-200 text-left md:text-center">Cuatro conceptos fundamentales para entender React</h2>
     </header>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
-import Image from "next/image";
-import Header from "@/components/header";
+import React from "react";
+import Header from "@/app/components/header";
+import componentsImg from "@/public/components.png";
 
 export default function Home() {
   return (
-    <Header>
-    <div className="font-sixtyfour">Hola a todos</div>
-    </Header>
+    <>
+      <Header />
+      <section id="conceptos" className="m-12 flex flex-col justify-center bg-violet-800 min-w-44">
+      <h1 className="text-2xl font-extrabold text-slate-100 p-3 mx-auto mb-6 overflow-clip">Conceptos básicos de React</h1>
+      <ul className="m-3 md:m-8 flex flex-col md:flex-row space-y-16 md:space-y-0 md:space-x-6">
+        <li className="flex flex-col items-center justify-center">
+          
+        </li>
+
+      </ul>
+      </section>
+    </>
   );
 }
