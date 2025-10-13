cf4fdd02081e4a4b43c637fbdeffd5c6687869ff
Olga Garcia <olgarciaq11@gmail.com>
2025-10-13 14:07:46 +0200

    TERMINADO PERO MAS CORTO

--- app/components/cor-concepts-list.tsx
+++ app/components/cor-concepts-list.tsx
@@ -12,7 +12,7 @@ export default function CoreConceptsList() {
     <section id="conceptos" className="m-12 flex flex-col justify-center bg-violet-800 min-w-44">
       <h1 className="text-2xl font-extrabold text-slate-100 p-3 mx-auto mb-6 overflow-clip">Conceptos básicos de React</h1>
       <ul className="m-3 md:m-8 flex flex-col md:flex-row space-y-16 md:space-y-0 md:space-x-6">
-      {CORE_CONCEPTS.map((cc) => <CoreConcepts key={cc.title} title={cc.title} image={cc.image} description={cc.description} />)}
+      {CORE_CONCEPTS.map((cc) => <CoreConcepts key={cc.title} {...cc} />)}
       </ul>
     </section>
   )
