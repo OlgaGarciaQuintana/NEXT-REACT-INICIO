ca567f0873be3c29f3f160340c544d71786be395
Olga Garcia <olgarciaq11@gmail.com>
2025-10-20 13:46:31 +0200

    PUNTO DE CONTROL 2 HECHO

--- app/components/Tab/tab-example-section.tsx
+++ app/components/Tab/tab-example-section.tsx
@@ -7,17 +7,38 @@ import { EXAMPLES } from '@/public/data';
 
 export default function TabExampleSection() {
   const [selected, setSelected] = useState<keyof typeof EXAMPLES | null>(null);
-  function handleClick() {
-
+  function handleClick(tab: keyof typeof EXAMPLES) {
+    console.log("Han hecho click");
+    setSelected(tab)
   }
   return (
     <section id="tab-section" className= "my-12 md:mx-12 mx-4 flex flex-col">
       <div className="flex justify-start space-x-2">
-        <TabButton isSelected={selected === "components"} onClick={() => handleClick}>{EXAMPLES.components.title}</TabButton>
-        <TabButton isSelected={selected === "jsx"} onClick={() => handleClick}>{EXAMPLES.jsx.title}</TabButton>
-        <TabButton isSelected={selected === "props"} onClick={() => handleClick}>{EXAMPLES.props.title}</TabButton>
-        <TabButton isSelected={selected === "state"} onClick={() => handleClick}>{EXAMPLES.state.title}</TabButton>
+        <TabButton isSelected={selected === "components"} onClick={() => handleClick("components")}>{EXAMPLES.components.title}</TabButton>
+        <TabButton isSelected={selected === "jsx"} onClick={() => handleClick("jsx")}>{EXAMPLES.jsx.title}</TabButton>
+        <TabButton isSelected={selected === "props"} onClick={() => handleClick("props")}>{EXAMPLES.props.title}</TabButton>
+        <TabButton isSelected={selected === "state"} onClick={() => handleClick("state")}>{EXAMPLES.state.title}</TabButton>
       </div>
+      {
+        !selected && <p className="text-slate-200 p-3">Selecciona un ejemplo para ver el código</p>
+      }
+      {
+        !!selected &&
+        <div className="bg-slate-600 text-slate-200 p-12 m-3">
+          <h3 className="mb-10 text-lg">
+            {EXAMPLES[selected].title}
+          </h3>
+          <p className="mb-10">
+            {EXAMPLES[selected].description}
+          </p>
+          <hr/>
+          <pre className="mt-3 overflow-hidden">
+            <code>
+              {EXAMPLES[selected].code}
+            </code>
+          </pre>
+        </div>
+      }
 
       <TabExample/>
     </section>
